dc3255e4a972389e4c448a5df7d084653f082536
Rahul Kumawat <116284606+kumawat30rahul@users.noreply.github.com>
2023-03-07 16:38:18 +0530

    dg

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,15 @@
-// write code for Welcome component here
+import React from "react";
+import '../styles/App.css';
+
+const Welcome = ({name}) => {
+  return (
+    <>
+        <h1>"Hey! {name}"</h1>
+        <h2>"Welcome to school."</h2>
+    </>
+    
+  )
+}
+
+
+export default Welcome;
